d804b4f3757bc044f3622dbb61148cd5ffb1085a
Cody <codyyandura@gmail.com>
2025-09-26 22:42:44 -0400

    Small fixes

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,7 +5,7 @@ import ContactFooter from '../components/ContactFooter';
 const Contact: React.FC = () => (
   <Box>
     <Typography variant="h3" className="tabHeader" sx={{mb:2}}>
-      Contact Us
+      Contact Me
     </Typography>
     <img src="images\ContactImage.jpg" alt="Connie" style={{ width: '100%', maxWidth: '500px', height: 'auto' }} />
     <ContactFooter/>
